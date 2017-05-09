b33d0a42167b610d3797d1b44f37e1fcf9a47d01
Pierre-Elliott Thiboud <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-05-09 17:47:59 +0200

    Level 1 works
    The json level's description works great
    Only one problem left : when obsel's color are changing/changed

--- js/loader.js
+++ js/loader.js
@@ -19,45 +19,31 @@ function loadLevel() {
 // Representation of level 0, the json doesn't work yet
 // This level is hardcoded for testing purposes
 var leveltest = {
-    "buttons": [
+    buttons: [
         {
             id: "btn1",
-            shape: SQUARE
+            shape: 2
         },
         {
             id: "btn2",
-            shape: CIRCLE
+            shape: 1
+        },
+		{
+            id: "btn3",
+            shape: 3
         }
     ],
-    "stateMachine": {
+    stateMachine: {
         "e1": [
-            {
-                event: "btn1",
-                to: "e1",
-                action: {
-					f: "createObsel",
-					a: {
-						group: "btn1",
-						shape: SQUARE,
-						color: ORANGE,
-						valence: 0
-					}
-				}
-            },
-			{
-				event: "btn2",
-                to: "e1",
-                action: {
-					f: "createObsel",
-					a: {
-						group: "btn2",
-						shape: CIRCLE,
-						color: GREEN,
-						valence: 1
-					}
-				}
-			}
+            { event: "btn1", to: "e1", action: {f: "createObsel", a: {group: "btn1", shape: 2, color: 3, valence: -1} } },
+			{ event: "btn2", to: "e2", action: {f: "createObsel", a: {group: "btn2", shape: 1, color: 5, valence: 0} } },
+			{ event: "btn3", to: "e1", action: {f: "createObsel", a: {group: "btn3", shape: 3, color: 2, valence: 1} } }
         ],
+		"e2": [
+			{ event: "btn1", to: "e2", action: {f: "createObsel", a: {group: "btn1", shape: 2, color: 2, valence: 1} } },
+			{ event: "btn2", to: "e1", action: {f: "createObsel", a: {group: "btn2", shape: 1, color: 5, valence: 0} } },
+			{ event: "btn3", to: "e2", action: {f: "createObsel", a: {group: "btn3", shape: 3, color: 3, valence: -1} } }
+		],
 
 		"initial": "e1"
     }
