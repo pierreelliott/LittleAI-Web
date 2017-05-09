5396d2d78be8fa8f5975f21ece03ba4daf0a2b28
Pierre-Elliott Thiboud <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-05-10 00:03:48 +0200

    Level creation (+ menu) now dynamic
    Menu is created dynamically, depending of the file "levels.json", which describe each group and the levels it contains
    The menu works correctly (ie, levels' buttons properly load the level)
    The game load with the level 0
    
    Added a function to reset the playground
    Changed menu creation functions
    Removed static tab panel in the index (it is now created dynamically)
    Moved levels 0 and 1 to subfolder for better sorting
    Created "levels.json" to describe each category and the levels inside

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,11 @@
 window.addEventListener("load", function () {
-	//loadLevel();
-	ajax("levels/level001.json");
+	ajax("levels/group1/level_0.json", loadLevel);
 });
 
 function loadLevel(level) {
 	var levelsButtons, levelsFsm;
+
+	resetPlayground();
 
 	levelsButtons = level.buttons;
 	levelsFsm = level.stateMachine;
@@ -16,7 +17,38 @@ function loadLevel(level) {
 	}
 }
 
-function ajax(url) {
+/**
+ * resetPlayground - Remove all objects in the trace, reset the score, remove the commands
+ *
+ * @returns {type}  description
+ */
+function resetPlayground() {
+	var score = document.getElementById("score"),
+		trace = document.getElementById("traceContainer"),
+		commands = document.getElementById("commands"),
+		commandtip = document.getElementById("commandtip");
+
+	score.textContent = "0";
+	if(score.classList.contains("white")) { score.classList.toggle("white"); }
+	if(score.classList.contains("red")) { score.classList.toggle("red"); }
+	if(score.classList.contains("green")) { score.classList.toggle("green"); }
+
+	trace.textContent = "";
+
+	commands.textContent = "";
+	commands.append(commandtip);
+
+	/* To do : reset informations panel + world panel */
+}
+
+/**
+ * ajax - Do ajax call. Retrieve the JSON at the url and send it to the callback function
+ *
+ * @param  {type} url      Relative URL where take the json
+ * @param  {type} callback The function to use (and pass the file to) when the file is loaded
+ * @returns {type}          description
+ */
+function ajax(url, callback) {
 	var req = new XMLHttpRequest();
 	req.open("GET", url);
 	req.onerror = function() {
@@ -25,8 +57,7 @@ function ajax(url) {
 	req.onload = function() {
 		if (req.status === 200) {
 			var data = JSON.parse(req.responseText);
-			//callback(data);
-			loadLevel(data);
+			callback(data);
 		} else {
 			console.log("Erreur " + req.status);
 		}
