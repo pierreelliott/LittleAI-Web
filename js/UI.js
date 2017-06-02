d476d2c803d1b7d43253a42ed97fc7b591a4df83
Pierre-Elliott Thiboud <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-06-02 18:38:22 +0200

    Added buttons to open the infoPanel
    The infoPanel is now hidden by default

--- js/UI.js
+++ js/UI.js
@@ -1,3 +1,11 @@
 function closeInfoPanel() {
 	document.getElementById("infoPanel").style.display = "none";
 }
+
+function openInfoPanel() {
+	document.getElementById("infoPanel").style.display = "block";
+}
+
+function openReplayMode() {
+	document.getElementById("infoPanel").style.display = "block";
+}
